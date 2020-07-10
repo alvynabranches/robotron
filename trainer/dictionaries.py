1b1af8012c46ceb879eaf91f9c17afce5e1cbe47
Alvyn Abranches <alvynabranches@gmail.com>
2020-07-10 22:26:19 +0530

    10072020v5

--- trainer/dictionaries.py
+++ trainer/dictionaries.py
@@ -3,39 +3,67 @@
 # Dictonary are mutable
 
 # -> Defining an empty dictonary
-
+a = {}
+print(type(a))
+print(a)
+b = dict()
+print(type(b))
+print(b)
 
 # -> Defining a dictonary with single key-value pairs
-
+c = {'name': 'Alvyn Abranches'}
+print(c)
+d = dict(name='Alvyn Abranches')
+print(d)
 
 # -> Defining a dictonary with multiple key-value pairs
-
+e = {'firstname': 'Alvyn', 'lastname': 'Abranches'}
+print(e)
+f = dict(firstname='Alvyn', lastname='Abranches')
+print(f)
 
 # -> Defining mixed datatype for value
-
+g = {'name': 'Alvyn Abranches', 'age': 23, 'height':6.2}
+print(g)
+h = {'name': ['Manoj', 'Rajesh', 'Promod'], 'age': [26, 32, 42], 'height': (5.10, 5.8, 6.)}
+print(h)
 
 # -> Defining mixed datatype for key
-# NOTE: Supported types are only strings, numbers, 
-
+# NOTE: Supported types are only strings, numbers
+i = {1: 'Hello', 2.25: 'This', 'word': 'is', 'word2': 'an example'}
+print(i)
 
 # -> Nested dictonaries
-
+j = {'name': {'firstname': 'Promod', 'lastname': 'Sawant'}, 'age': 42}
+print(j)
 
 # -> List as value in a dictonary
-
+# we have seen this above
 
 # -> finding the length of a dictonary
-
+length = len(j)
+print(length)
 
 # -> iterating over a dictonary
+for z in g:
+    print(z) # this prints out the keys
 
+for z in g:
+    print(g[z])
 
 # -> adding new data to a existing dictonary
-
+print(g)
+g['weight'] = 65
+print(g)
 
 # -> iterating over a dictonary nested with another dictonary
+print(j)
+print(j['name']['firstname'])
 
-
+# -> other methods
+print(g.values()) # displays all the values
+print(g.get('name'))
+print(g.items())
 
 # End of this topic
 # Assignment for this topic -> S2A6
