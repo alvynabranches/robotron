da5a3790139bb86aad757bf9cbb65b0a2b90291d
Alvyn Abranches <alvynabranches@gmail.com>
2020-07-11 10:32:24 +0530

    11072020v1

--- trainer/functions.py
+++ trainer/functions.py
@@ -5,40 +5,105 @@
 # -> Built in functions
 # print
 # sum
-
+print(sum([10, 20, 30, 40, 50]))
 
 # -> User defined functions
 # Defining a user defined function
+def fun():
+    print('This is a function print statement')
 
 # calling a user defined function
-
+fun()
 
 # -> return statement
+print(fun())
 
+def retfun():
+    print('In the function')
+    return 'Hello'
+
+retfun()
+print(retfun())
 
 # functions with parameters
+def paramfun(username):
+    print('Hello ' + username)
 
+paramfun('Alvyn')
+
+def greeting(username):
+    return 'Hello ' + username
+
+print(greeting('Alvyn'))
 
 # functions with more than 1 return variable
+def more_parameters(a, b):
+    return a, b
 
+print(more_parameters(1, 2))
+
+a, b = more_parameters(1, 2)
+print(a)
+print(b)
 
 # functions with default parameters
+def defaultparam(username='Alvyn'):
+    return 'Hello ' + username
 
+c = defaultparam()
+d = defaultparam('Manohar')
+print(c)
+print(d)
 
 # functions with parameters and default parameters
+def mixedparameters(name, message='Good morning'):
+    print(message + ' ' + name)
 
+mixedparameters('Alvyn')
 
 # variable length arguments
-# def fun(*var_tuple):
-#     for var in var_tuple:
-#         print(var)
-# fun('Alvyn', 23)
+def fun(*var_tuple):
+    for var in var_tuple:
+        print(var)
+
+fun('Alvyn', 23)
 
 # scope of the variable => local variables, global variables
+a = 5
 
+def increment(a):
+    a += 1
+    print(a)
+increment(20)
 
+print(a)
+
+def increment2():
+    a += 1 # will return a error because a was not defined inside the function
+    print(a)
+# increment2()
+
+def increment3():
+    global a
+    a += 1
+    print(a)
+    
+print(a)
+
+increment3()
 # Recursion => PPT
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n-1)
 
+print(factorial(5))
+
+
+# Blank function
+def fun():
+    pass
 
 # End of the topic
 # Assignment for this topic -> S2A7
