95c6eaf9aac74a44b83880acb61714b445cc40b4
Alvyn Abranches <alvynabranches@gmail.com>
2020-07-13 12:17:51 +0530

    13072020v1

--- student/more_concepts.py
+++ student/more_concepts.py
@@ -19,6 +19,22 @@
 # -> Lists can take in dictonaries and sets
 
 
+# -> List comprehension
+
+
+# -> List comprehension with if
+
+
+# -> Assign string with if else condition
+
+
+# -> List comprehension with if else
+x = [i for i in range(1, 11)]
+print(x)
+y = ['even' if (i % 2 == 0) else 'odd' for i in x]
+print(y)
+
+# -> 
 
 # End of the topic
 # Assignment for this topic
---
+++ student/try.py
@@ -0,0 +1,8 @@
+i = 25
+f = 100.25
+s = 'Hi'
+
+f_string = s + str(i)
+print(f_string)
+f_string = f"{s}{i}"
+print(f_string)
